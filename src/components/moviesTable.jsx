513f30d16bda058796948410f6d8cfa754139389
tammamakhan <tammam.a.khan@gmail.com>
2021-11-09 14:25:02 -0800

    Authorization implementation complete

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Like from "./common/like";
 import Table from "./common/table";
+import auth from "../services/authService";
 
 class MoviesTable extends Component {
   columns = [
@@ -36,6 +37,9 @@ class MoviesTable extends Component {
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
+    const user = auth.getCurrentUser();
+
+    if (!user || !user.isAdmin) this.columns.pop();
 
     return (
       <Table
